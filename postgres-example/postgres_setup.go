a1eff8dda8b11c44ad3434570aa46aeada230552
Robert Mascitelli <robertmascitelli@gmail.com>
2023-10-24 20:12:49 -0400

    fix typo

--- postgres-example/postgres_setup.go
+++ postgres-example/postgres_setup.go
@@ -22,6 +22,9 @@ const (
     user     = "postgres"
     password = "new_password"
     dbname   = "testdb"
+    tablename = "example"
+
+    TABLE_EXIST = "SELECT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'example') AS table_existence;"
 )
 
 func drop_table(conninfo string) {
@@ -32,7 +35,7 @@ func drop_table(conninfo string) {
     }
     defer db.Close()
 
-    _, err = db.Exec("DROP TABLE second_example")
+    _, err = db.Exec(fmt.Sprintf("DROP TABLE %s", tablename))
     if err != nil {
         log.Fatal(err)
     }
@@ -46,7 +49,7 @@ func create_table(conninfo string) {
     }
     defer db.Close()
 
-    _, err = db.Exec("CREATE TABLE second_example ( id integer, username varchar(255) )")
+    _, err = db.Exec("CREATE TABLE %s ( id integer, username varchar(255) )", tablename)
     if err != nil {
         log.Fatal(err)
     }
@@ -65,10 +68,29 @@ func create_db(conninfo string) {
         log.Fatal(err)
     }
 }
+
+func query_table(conninfo string) {
+    conninfo = conninfo + fmt.Sprintf(" dbname=%s", dbname)
+    db, err := sql.Open("postgres", conninfo)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    //r, err := db.Exec(fmt.Sprintf("SELECT * FROM %s;", tablename))
+    r, err := db.Exec("SELECT * FROM fake_table")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("r = %v\n", r)
+}
  
 func main() {
     conninfo := fmt.Sprintf("user=%s password=%s host=%s sslmode=disable", user, password, host)
     // create_db(conninfo)
     //create_table(conninfo)
-    drop_table(conninfo)
+    //drop_table(conninfo)
+
+    query_table(conninfo)
 }
